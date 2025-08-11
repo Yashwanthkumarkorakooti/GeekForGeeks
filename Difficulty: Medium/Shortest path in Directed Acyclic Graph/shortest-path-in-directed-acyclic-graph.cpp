34ef1522aa4be11f213f0900bad124219237cfd2
YashwanthKumar <141126524+Yashwanthkumarkorakooti@users.noreply.github.com>
2025-08-11 09:35:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -1,50 +1,47 @@
 // User function Template for C++
 class Solution {
   public:
-    void dfs(int node,vector<vector<pair<int,int>>>&adj,vector<int>&visited,stack<int>&stk){
+    void dfs(int node,vector<vector<pair<int,int>>> &adj,vector<int>&visited, stack<int>&stk){
         visited[node] = 1 ;
-        for(auto& k:adj[node]){
-           int neighbour = k.first ;
-           if(!visited[neighbour]){
-               dfs(neighbour,adj,visited,stk);
-           }
+        for(auto& k : adj[node]){
+            int neighbour = k.first;
+            if(!visited[neighbour]){
+                dfs(neighbour,adj,visited,stk);
+            }
         }
         stk.push(node);
     }
-    
     vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
-        vector<int> visited(V,0);
+        vector<int>visited(V,0);
         vector<vector<pair<int,int>>> adj(V);
         
-        for(auto& e:edges){
-            adj[e[0]].push_back({e[1],e[2]});
+        for(auto& edge:edges){
+            adj[edge[0]].push_back({edge[1],edge[2]});
         }
         
         stack<int> stk;
         dfs(0,adj,visited,stk);
         
-        vector<int> dist(V,INT_MAX);
+        vector<int>dist(V,INT_MAX);
         dist[0] = 0 ;
         
         while(!stk.empty()){
             int cur = stk.top();
             stk.pop();
             if(dist[cur] != INT_MAX){
-                for(auto& k: adj[cur]){
-                    int neighbour = k.first ;
+                for(auto& k : adj[cur]){
+                    int neighbour = k.first;
                     int weight = k.second;
-                    if(dist[cur] + weight < dist[neighbour]){
+                    if(dist[cur]+ weight < dist[neighbour]){
                         dist[neighbour] = dist[cur] + weight;
                     }
                 }
             }
         }
-        
         for(int i=0;i<V;i++){
-            if(dist[i] == INT_MAX){
-                dist[i] = -1 ;
-            }
+            if(dist[i] == INT_MAX) dist[i] = -1 ;
         }
+        
         return dist;
     }
 };
